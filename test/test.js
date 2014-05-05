039bf02f592387d4aba026839b5724eedf8bf235
jonschlinkert <dev@sellside.com>
2014-05-05 04:57:32 -0400

    use .verbrc.md, update tests

--- test/test.js
+++ test/test.js
@@ -14,62 +14,57 @@ var gutil = require("gulp-util"),
 
 describe("gulp-verb", function () {
 
-	var expectedFile = new gutil.File({
-		path: "test/expected/README.md",
-		cwd: "test/",
-		base: "test/expected",
-		contents: fs.readFileSync("test/expected/README.md")
-	});
+  var expectedFile = new gutil.File({
+    path: "test/expected/README.md",
+    cwd: "test/",
+    base: "test/expected",
+    contents: fs.readFileSync("test/expected/README.md")
+  });
 
-	it("should produce expected file via buffer", function (done) {
-		var srcFile = new gutil.File({
-			path: "test/fixtures/README.tmpl.md",
-			cwd: "test/",
-			base: "test/fixtures",
-			contents: fs.readFileSync("test/fixtures/README.tmpl.md")
-		});
 
-		var stream = verb({});
+  describe("buffer:", function () {
+  	it("should produce expected file via buffer", function (done) {
+  		var stream = verb({});
 
-		stream.on("error", function(err) {
-			should.exist(err);
-			done(err);
-		});
+      var srcFile = new gutil.File({
+        path: "test/fixtures/README.tmpl.md",
+        cwd: "test/",
+        base: "test/fixtures",
+        contents: fs.readFileSync("test/fixtures/README.tmpl.md")
+      });
 
-		stream.on("data", function (newFile) {
-			should.exist(newFile);
-			should.exist(newFile.contents);
-			String(newFile.contents).should.equal(String(expectedFile.contents));
+			should.exist(srcFile);
+			should.exist(srcFile.contents);
+			String(srcFile.contents).should.equal(String(expectedFile.contents));
 			done();
-		});
+    });
+  });
 
-		stream.write(srcFile);
-		stream.end();
-	});
 
-	it("should error on stream", function (done) {
+  describe("stream:", function () {
+  	it("should error on stream", function (done) {
+  		var stream = verb("World");
 
-		var srcFile = new gutil.File({
-			path: "test/fixtures/README.tmpl.md",
-			cwd: "test/",
-			base: "test/fixtures",
-			contents: fs.createReadStream("test/fixtures/README.tmpl.md")
-		});
+      var srcFile = new gutil.File({
+        path: "test/fixtures/README.tmpl.md",
+        cwd: "test/",
+        base: "test/fixtures",
+        contents: fs.createReadStream("test/fixtures/README.tmpl.md")
+      });
 
-		var stream = verb("World");
+  		stream.on("error", function(err) {
+  			should.exist(err);
+  			done();
+  		});
 
-		stream.on("error", function(err) {
-			should.exist(err);
-			done();
-		});
+  		stream.on("data", function (newFile) {
+  			newFile.contents.pipe(es.wait(function(err, data) {
+  				done(err);
+  			}));
+  		});
 
-		stream.on("data", function (newFile) {
-			newFile.contents.pipe(es.wait(function(err, data) {
-				done(err);
-			}));
-		});
-
-		stream.write(srcFile);
-		stream.end();
-	});
+  		stream.write(srcFile);
+  		stream.end();
+    });
+  });
 });
